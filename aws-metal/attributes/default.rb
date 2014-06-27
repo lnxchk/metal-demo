9aedb8448ec75ca9ac67a90bacf7e374f895110f
Mandi Walls <mandi.walls@gmail.com>
2014-06-26 23:58:52 -0700

    don't need the attrs for aws-metal

--- aws-metal/attributes/default.rb
+++
@@ -1,4 +0,0 @@
-default['aws-metal']['chef-server'] = "https://api.opscode.com/organizations/mandi_demos"
-default['aws-metal']['client_name'] = "lnxchk"
-default['aws-metal']['client_key'] = "/etc/chef/lnxchk.pem"
-
